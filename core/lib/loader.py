b131e69b49ecc9ee28f669a6f8cecc88f10b9d3d
Thanh Le <hi@thanhle.xyz>
2025-08-03 17:42:25 +0900

    x

--- core/lib/loader.py
+++ core/lib/loader.py
@@ -9,9 +9,9 @@ def create(args):
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,))  # Normalize the images
     ])
-    # Load the MNIST dataset
-    train_dataset = datasets.MNIST(root=args.data_dir, train=True, download=True, transform=transform)
+    # Load the FashionMNIST dataset
+    train_dataset = datasets.FashionMNIST(root=args.data_dir, train=True, download=True, transform=transform)
     train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=True)
-    test_dataset = datasets.MNIST(root=args.data_dir, train=False, download=True, transform=transform)
+    test_dataset = datasets.FashionMNIST(root=args.data_dir, train=False, download=True, transform=transform)
     test_loader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=False)
     return train_loader, test_loader
